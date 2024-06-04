bfe8b25d640d91e095463ee06ff026997ae19764
tadeowoto <tadeo.woto@gmail.com>
2024-06-04 17:19:12 -0300

    primera parte de las pruebas del main :)

--- SolBus_grupo1/src/Main/SolBusMain.java
+++ SolBus_grupo1/src/Main/SolBusMain.java
@@ -6,7 +6,75 @@ public class SolBusMain {
    
     public static void main(String[] args) {
        
+        //        ColectivoData cd = new ColectivoData();
+//        HorarioData hd =  new HorarioData();
+//        PasajeData pd = new PasajeData();
+//        PasajeroData pjd = new PasajeroData();
+//        RutaData rd = new RutaData();
+//        ArrayList<Colectivo> colectivos = new ArrayList<>();
+//        ArrayList<Ruta> rutas = new ArrayList<>();
+//        ArrayList<Horario> horarios = new ArrayList<>();
+        //PRUEBAS COLECTIVODATA
+        //String matricula, String marca, String modelo, int capacidad
+//      Colectivo colectivo = new Colectivo(14,"PEDRO", "Marco Polo","Mercedes 1", 10);
+//      Colectivo reemplazo = new Colectivo("AGUANTE SAN LORENZO","Marco Polo","Mercedes 2",5);
+//      cd.guardarColectivo(colectivo);  
+//      cd.borrarColectivo(14);
+//      System.out.println(cd.buscarColectivoPorId(13).toString());
+//        cd.actualizarColectivo(colectivo);
+//        colectivos = (ArrayList<Colectivo>) cd.listarColectivos();
+//        for (Colectivo colectivo1 : colectivos) {
+//            System.out.println(colectivo1.toString());
+//        }
+//       cd.eliminarColectivo(colectivo);
+
+//        rd.guardarRuta(r);
+//        rd.guardarRuta(r2);
+//        rd.bajaRuta(2);
+//        System.out.println(rd.buscarRutaPorID(2).toString());
         
+//        rd.actualizarRuta(r2);
+        
+        //PRUEBAS HORARIODATA
+//        
+//        Horario h = new Horario(8,r2, LocalTime.of(11, 00), LocalTime.of(17, 40), true);
+        
+//        hd.guardarHorario(h);
+//        hd.borrarHorarioFisico(6);
+//        hd.bajaHorario(7);
+//        System.out.println(hd.buscarHorarioPorId(8).toString());
+//          hd.actualizarHorario(h);
+//           horarios = (ArrayList<Horario>) hd.listarHorarios();
+//        for (Horario aux : horarios) {
+//               System.out.println(aux.toString());
+//        }
+
+//        horarios = (ArrayList<Horario>) hd.listarHorariosPorHoraSalida(LocalTime.of(11, 00));
+//        for (Horario aux : horarios) {
+//               System.out.println(aux.toString());
+////        }
+//        horarios = (ArrayList<Horario>) hd.listarHorariosPorRuta(r2);
+//        for (Horario aux : horarios) {
+//               System.out.println(aux.toString());
+//        }
+
+//        PRUEBAS RUTADATA
+//        Ruta r = new Ruta("Merlo", "San Luis", LocalTime.of(3, 40), true);
+//        Ruta r2 = new Ruta( 1, "guanacoBoleado", "mercedes", LocalTime.of(12, 00), true);
+//        rutas = (ArrayList<Ruta>) rd.listarRutasDisponibles();
+//        for (Ruta aux : rutas) {
+//               System.out.println(aux.toString());
+//        }
+//        
+//        rutas = (ArrayList<Ruta>) rd.listarRutasPorDestino("DEPRESION");
+//        for (Ruta aux : rutas) {
+//               System.out.println(aux.toString());
+//        }
+//        
+//        rutas = (ArrayList<Ruta>) rd.listarRutasPorOrigen("PROGRAMACION 2");
+//        for (Ruta aux : rutas) {
+//               System.out.println(aux.toString());
+//        }
         
     }
     
