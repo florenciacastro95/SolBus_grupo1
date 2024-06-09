1f33c8d77d7795e06b598de3a3a320df60770e4b
Adan-Coronel <adanjhoel@gmail.com>
2024-06-09 20:36:14 -0300

    correccion en conexiondata

--- SolBus_grupo1/src/accesoDatos/Conexion.java
+++ SolBus_grupo1/src/accesoDatos/Conexion.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 public class Conexion {
-    private static final String URL = "jdbc:mariadb://localhost:3336/";
+    private static final String URL = "jdbc:mariadb://localhost/";
     private static final String DB = "solbusdbcargada";
     private static final String USER ="root";
     private static final String PASSWORD = "";
