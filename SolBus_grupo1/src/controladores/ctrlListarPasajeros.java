ada635ae56f644df83ebc5aa25d2277a9dbde9d7
tadeowoto <tadeo.woto@gmail.com>
2024-06-10 12:27:04 -0300

    agregando metodos de listar pasajeros

--- SolBus_grupo1/src/controladores/ctrlListarPasajeros.java
+++ SolBus_grupo1/src/controladores/ctrlListarPasajeros.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
@@ -20,13 +21,13 @@ import vistas.*;
  *
  * @author tDev
  */
-public class ctrlListarPasajeros implements ActionListener{
+public class ctrlListarPasajeros implements ActionListener {
 
     private Pasajero pasajero;
     private PasajeroData pasajeroData;
     private InfListaBajaPasajeros pasajeroVista;
     private DefaultTableModel model = new DefaultTableModel();
-    
+
     public ctrlListarPasajeros(Pasajero pasajero, PasajeroData pasajeroData, InfListaBajaPasajeros pasajeroVista) {
         this.pasajero = pasajero;
         this.pasajeroData = pasajeroData;
@@ -35,11 +36,14 @@ public class ctrlListarPasajeros implements ActionListener{
         pasajeroVista.rbNombreApellido.addActionListener(this);
         pasajeroVista.rbVerTodo.addActionListener(this);
         pasajeroVista.rbDni.addActionListener(this);
+        pasajeroVista.btnBorrar.addActionListener(this);
+        pasajeroVista.btnRegistrar.addActionListener(this);
+        pasajeroVista.btnActualizar.addActionListener(this);
         armarCabecera();
         addDocumentListeners();
-        
+
     }
-    
+
     private void armarCabecera() {
         ArrayList<Object> filaCabecera = new ArrayList<>();
         filaCabecera.add("Nombre");
@@ -47,17 +51,16 @@ public class ctrlListarPasajeros implements ActionListener{
         filaCabecera.add("Dni");
         filaCabecera.add("Correo");
         filaCabecera.add("Telefono");
-        
 
         for (Object i : filaCabecera) {
             model.addColumn(i);
         }
         pasajeroVista.jtListarPasajeros.setModel(model);
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
         if (e.getSource() == pasajeroVista.rbDni) {
             pasajeroVista.jtNombre.setEnabled(false);
             pasajeroVista.jtDni.setEnabled(true);
@@ -70,14 +73,105 @@ public class ctrlListarPasajeros implements ActionListener{
             pasajeroVista.jtNombre.setEnabled(false);
             pasajeroVista.jtDni.setEnabled(false);
             actualizarTablaConPasajeros((ArrayList<Pasajero>) pasajeroData.listarPasajeros());
-            
+
         }
-        
-        
-        
-        
+        if (e.getSource() == pasajeroVista.btnBorrar) {
+            int selectedRow = pasajeroVista.jtListarPasajeros.getSelectedRow();
+            if (selectedRow != -1) {
+                String nombre = (String) pasajeroVista.jtListarPasajeros.getValueAt(selectedRow, 0);
+                String apellido = (String) pasajeroVista.jtListarPasajeros.getValueAt(selectedRow, 1);
+
+                Pasajero pasajeroAEliminar = pasajeroData.buscarPasajeroPorApellido(apellido);
+                if (pasajeroAEliminar != null) {
+                    int confirm = JOptionPane.showConfirmDialog(pasajeroVista,
+                            "¿Está seguro que desea borrar al pasajero " + nombre + " " + apellido + "?",
+                            "Confirmar Borrado",
+                            JOptionPane.YES_NO_OPTION);
+
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        pasajeroData.borrarPasajero(pasajeroAEliminar.getIdPasajero());
+                        model.removeRow(selectedRow);
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(pasajeroVista, "No se encontró el pasajero.");
+                }
+            } else {
+                JOptionPane.showMessageDialog(pasajeroVista, "Seleccione un pasajero para borrar.");
+            }
+        }
+
+        if (e.getSource() == pasajeroVista.btnRegistrar) {
+            String nombre = pasajeroVista.txtNombre.getText();
+            String apellido = pasajeroVista.txtApellido.getText();
+            String dniText = pasajeroVista.txtDni.getText();
+            String correo = pasajeroVista.txtCorreo.getText();
+            String telefono = pasajeroVista.jTextField5.getText();
+
+            if (!validarString(nombre)) {
+                JOptionPane.showMessageDialog(pasajeroVista, "Nombre inválido.");
+               
+            }
+
+            if (!validarString(apellido)) {
+                JOptionPane.showMessageDialog(pasajeroVista, "Apellido inválido.");
+               
+            }
+
+            if (!dniText.matches("\\d+") || !validarDniTam(dniText.length())) {
+                JOptionPane.showMessageDialog(pasajeroVista, "DNI inválido.");
+                
+            }
+
+            Pasajero nuevoPasajero = new Pasajero();
+            nuevoPasajero.setNombre(nombre);
+            nuevoPasajero.setApellido(apellido);
+            nuevoPasajero.setDni(dniText);
+            if (!correo.isEmpty()) {
+                nuevoPasajero.setCorreo(correo);
+            }
+            if (!telefono.isEmpty()) {
+                nuevoPasajero.setTelefono(telefono);
+            }
+
+            pasajeroData.guardarPasajero(nuevoPasajero);
+            actualizarTablaConPasajeros((ArrayList<Pasajero>) pasajeroData.listarPasajeros());
+
+            pasajeroVista.txtNombre.setText("");
+            pasajeroVista.txtApellido.setText("");
+            pasajeroVista.txtDni.setText("");
+            pasajeroVista.txtCorreo.setText("");
+            pasajeroVista.jTextField5.setText("");
+        }
+
+        if (e.getSource() == pasajeroVista.btnActualizar) {
+            int filaSeleccionada = pasajeroVista.jtListarPasajeros.getSelectedRow();
+            if (filaSeleccionada != -1) {
+                String dni = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 2);
+                 System.out.println("DNI seleccionado: " + dni);
+                Pasajero pasajero = pasajeroData.buscarPasajeroPorDni(dni);
+
+                if (pasajero != null) {
+                    int idPasajero = pasajero.getIdPasajero();
+
+                    String nuevoNombre = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 0);
+                    String nuevoApellido = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 1);
+                    String nuevoDni = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 2);
+                    String nuevoCorreo = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 3);
+                    String nuevoTelefono = (String) pasajeroVista.jtListarPasajeros.getValueAt(filaSeleccionada, 4);
+                    Pasajero pasajeroActualizado = new Pasajero(idPasajero, nuevoNombre, nuevoApellido, nuevoDni, nuevoCorreo, nuevoTelefono);
+                    pasajeroData.actualizarPasajero(pasajeroActualizado);
+                }
+
+            } else {
+                JOptionPane.showMessageDialog(pasajeroVista, "Seleccione una fila para actualizar.");
+            }
+        }
+
+
+
+
     }
-    
+
     
     public void cargarTabla(Pasajero p){
     
